0d77d0b723a6d80593b3d5812555667e39cd0c6a
Timothy Tampöld <timothytampold@gmail.com>
2022-07-31 05:07:20 +0200

    Set up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      manrope: ['Manrope', 'sans-serif'],
+    },
+    fontWeight: {
+      base: 800,
+    },
+    colors: {
+      'cyan-light': 'hsl(193, 38%, 86%)',
+      'green-neon': 'hsl(150, 100%, 66%)',
+      'blue-grayish': 'hsl(217, 19%, 38%)',
+      'blue-grayish-dark': 'hsl(217, 19%, 24%)',
+      'blue-dark': 'hsl(218, 23%, 16%)',
+    },
     extend: {},
   },
   plugins: [],
